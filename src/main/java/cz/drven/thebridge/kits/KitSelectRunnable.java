cf8ed91a6fcc1dde26479f6aff3cd2d720f13674
JustDrven <petr.nemec11111@gmail.com>
2022-12-29 11:39:14 +0100

    Interfaces for Kits 🔑

---
+++ src/main/java/cz/drven/thebridge/kits/KitSelectRunnable.java
@@ -0,0 +1,9 @@
+package cz.drven.thebridge.kits;
+
+import org.bukkit.entity.Player;
+
+public interface KitSelectRunnable {
+
+    void onSelect(Player player, Kit kit);
+
+}
---
+++ src/main/java/cz/drven/thebridge/kits/KitUnselectRunnable.java
@@ -0,0 +1,9 @@
+package cz.drven.thebridge.kits;
+
+import org.bukkit.entity.Player;
+
+public interface KitUnselectRunnable {
+
+    void unselect(Kit kit, Player player);
+
+}
